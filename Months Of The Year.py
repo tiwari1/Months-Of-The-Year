6dd3c13b5d5ecf3154b71bea988a3b4f91c61301
Akshita Tiwari <85664297+tiwari1@users.noreply.github.com>
2024-03-27 23:56:02 +0530

    Add files via upload

---
+++ Months Of The Year.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#WAP to print months of the year
+x=int(input("enter the no.-->"))
+if x == 1:
+    print("jan")
+elif x == 2:
+    print("feb")
+elif x == 3:
+    print("march")
+elif x == 4:
+    print("april")
+elif x == 5:
+    print("may")
+elif x == 6:
+    print("june")
+elif x == 7:
+    print("july")
+elif x == 8:
+    print("aug")
+elif x == 9:
+    print("sept")
+elif x == 10:
+    print("oct")
+elif x == 11:
+    print("nov")
+elif x == 12:
+    print("dec")          
+else:
+    print("Wrong input")
+
+
+# In[ ]:
+
+
+
+
